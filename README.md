b1beef930ea71e38eb58c6e8997205f73aad8cab
Bruno M V Souza <git@bmvs.dev>
2023-10-12 12:08:48 -0300

    Add generics support (#2)

---
+++ README.md
@@ -0,0 +1,19 @@
+# Singleflight with Generics!
+
+[![GoDoc](https://godoc.org/github.com/brunomvsouza/singleflight?status.svg)](https://godoc.org/github.com/brunomvsouza/singleflight)
+
+> Package singleflight provides a duplicate function call suppression mechanism.
+
+This fork adds generics support (`Group[K comparable, V any]` and `Result[V any]`) to the original [x/sync/singleflight](https://golang.org/x/sync/singleflight) package.
+
+### Updates
+
+- I will keep this package up-to-date with the original one, at least until `x/sync/singleflight` adds support for generics. If you notice an update before I do, please open an issue or submit a pull request.
+- Versions will be tagged to align with the same versioning as the `x/sync/singleflight` package.
+
+### Usage
+
+For example usage, see:
+- [Group.Do](examples/Do/main.go)
+- [Group.DoChan](examples/DoChan/main.go)
+- [Group.DoForget](examples/Forget/main.go)
---
+++ examples/Forget/main.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2023 Bruno Marques Venceslau de Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/brunomvsouza/singleflight"
+)
+
+func main() {
+	var (
+		group singleflight.Group[string, string]
+		wg    sync.WaitGroup
+	)
+
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		res1, err1, shared1 := group.Do("key", func() (string, error) {
+			time.Sleep(1 * time.Second)
+			return "func 1", nil
+		})
+		fmt.Println("res1:", res1)
+		fmt.Println("err1:", err1)
+		fmt.Println("shared1:", shared1)
+	}()
+
+	go func() {
+		defer wg.Done()
+		res2, err2, shared2 := group.Do("key", func() (string, error) {
+			time.Sleep(1 * time.Second)
+			return "func 2", nil
+		})
+		fmt.Println("res2:", res2)
+		fmt.Println("err2:", err2)
+		fmt.Println("shared2:", shared2)
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(100 * time.Millisecond)
+		group.Forget("key")
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(200 * time.Millisecond)
+		res3, err3, shared3 := group.Do("key", func() (string, error) {
+			time.Sleep(1 * time.Second)
+			return "func 3", nil
+		})
+		fmt.Println("res3:", res3)
+		fmt.Println("err3:", err3)
+		fmt.Println("shared3:", shared3)
+	}()
+
+	wg.Wait()
+
+	// Output:
+	// res2: func 1
+	// err2: <nil>
+	// shared2: true
+	// res1: func 1
+	// err1: <nil>
+	// shared1: true
+	// res3: func 3
+	// err3: <nil>
+	// shared3: false
+}
